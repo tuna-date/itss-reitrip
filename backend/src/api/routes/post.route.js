4a86fe0264e00732afe09ff8171e9c5a3c0b289a
tuna <tu.nguyen@sotatek.com>
2020-06-16 05:32:37 +0700

    remove auth for some apis

--- backend/src/api/routes/post.route.js
+++ backend/src/api/routes/post.route.js
@@ -6,8 +6,8 @@ const route = Router();
 
 route.post('/places/:place_id/posts', [auth], postController.createPost);
 route.put('/places/:place_id/posts', [auth], postController.updatePost);
-route.get('/places/:place_id/posts/:id', [auth], postController.getPost);
-route.get('/places/:place_id/posts', [auth], postController.getListPosts);
+route.get('/places/:place_id/posts/:id', [], postController.getPost);
+route.get('/places/:place_id/posts', [], postController.getListPosts);
 route.delete('/places/:place_id/posts', [auth], postController.removePost);
 route.post('/places/:place_id/posts/:id/upvote', [auth], postController.upvotePost);
 route.get('/places/:place_id/currentUserPosts', [auth], postController.getCurrentUserPosts);
