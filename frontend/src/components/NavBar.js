07a15a9f4e069a96503a9eee4636771e63b01629
PhongPhamdt <phongpham.hedspi@gmail.com>
2020-06-18 11:55:03 +0700

    finish sign in sign out comment

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react'
-import { Menu } from 'antd'
+import { Menu, Button } from 'antd'
 import { Link } from 'react-router-dom'
+import API from 'utils/API'
 
 export default class NavBar extends Component {
+  constructor() {
+    super()
+    this.state = {
+      token: null,
+      user: null
+    }
+  }
+
+  async componentDidMount() {
+    let token = localStorage.getItem('token')
+    // if (token) {
+    let user = await API.get('/auth/currentUser', {
+      headers: {
+        Authorization: token
+      }
+    })
+    this.setState({ token: token, user: user.data })
+    // }
+  }
+
+  logout() {
+    localStorage.removeItem('token')
+    this.setState({ token: null, user: null })
+  }
+
   render() {
+    const { token, user } = this.state
+
     return (
       <div>
         <div style={{ width: 120, height: 31, background: 'rgba(255, 255, 255, 0.2)', margin: '16px 24px 16px 0', float: 'left', color: 'white' }} />
@@ -18,9 +46,26 @@ export default class NavBar extends Component {
               About
             </Link>
           </Menu.Item>
-          <Menu.Item style={{ float: 'right' }}>
-            Sign In
-          </Menu.Item>
+          {token ? <>
+            <Menu.Item
+              style={{ float: 'right', marginLeft: 30 }}
+            >
+              <Button type='danger' onClick={() => this.logout()}>
+                Log Out
+              </Button>
+            </Menu.Item>
+            <Menu.Item style={{ float: 'right' }}>
+              <Link to='/profile' >
+                {user.username}
+              </Link>
+            </Menu.Item>
+          </> :
+            <Menu.Item style={{ float: 'right' }}>
+              <Link to='/signin'>
+                Sign In
+              </Link>
+            </Menu.Item>
+          }
         </Menu>
       </div>
     )
