93089a7939aad71477daf0917c62513fb3404ddc
Dantokoro <duongdang0508@gmail.com>
2020-06-18 07:35:04 +0700

    post detail FE

--- frontend/src/pages/Post.js
+++ frontend/src/pages/Post.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import { Breadcrumb, Card, Avatar, Rate, Row, Col } from 'antd'
-import { UserOutlined } from '@ant-design/icons'
+import { Breadcrumb, Card, Avatar, Rate, Row, Col, Button, Form, Input} from 'antd'
+import { LikeOutlined } from '@ant-design/icons'
 import { Link } from 'react-router-dom'
 import API from 'utils/API'
 
@@ -11,7 +11,9 @@ export default class Post extends Component {
     super()
     this.state = {
       place: null,
-      posts: null
+      post: null,
+      post_owner: null,
+      comments: null
     }
   }
 
@@ -19,42 +21,50 @@ export default class Post extends Component {
     const { match: { params } } = this.props
     let places = await API.get(`/places/${params.id}`)
 
-    let postsData = await API.get(`/places/${params.id}/posts`)
-    for (let i = 0; i < postsData.data.length; i++) {
-      let user = await API.get(`/users/${postsData.data[i].userId}`)
-      postsData.data[i].user = user.data
+    let postDetail = await API.get(`/places/${params.id}/posts/${params.postid}`)
+    let post_owner = await API.get(`users/${postDetail.data.user_id}`)
+    let commentsData = await API.get(`/places/${params.id}/posts/${params.postid}/comments`)
+    for (let i = 0; i < commentsData.data.length; i++) {
+      let user = await API.get(`/users/${commentsData.data[i].userId}`)
+      commentsData.data[i].user = user.data
     }
-    this.setState({ place: places.data, posts: postsData.data })
+    this.setState({ place: places.data, post: postDetail.data, post_owner: post_owner.data, comments: commentsData.data })
+  }
+
+  upVote(){
+
   }
 
   render() {
-    const { place, posts } = this.state
-    console.log(this.state);
+    const { place, post, post_owner, comments } = this.state
+    console.log("Post", post, "Comment: ", comments);
     return (
       <div style={{ minHeight: 380, minWidth: 1000 }}>
         <Breadcrumb separator=">" style={{ margin: '16px 0px', fontSize: 20 }}>
           <Breadcrumb.Item><Link to='/'>Home</Link></Breadcrumb.Item>
-          <Breadcrumb.Item>Place</Breadcrumb.Item>
+          <Breadcrumb.Item>{place && place.name}</Breadcrumb.Item>
+
         </Breadcrumb>
-        <Card>
-          <Meta
-            avatar={<Avatar shape='square' size={100} src="https://res-4.cloudinary.com/enchanting/images/w_1600,h_700,c_fill,f_auto/et-web/2015/05/Enchanting-Travels-Vietnam-Tours-Nha-Trang-Hotels-Evason-Ana-Mandara-Nha-Trang-Hotel-in-Vietnam-Nha-Trang-beach-bbq/vietnam8217s-heritage-cities-and-beach-tour-trip-1.jpg" />}
-            title={place && place.name}
-            description={<div>
-              <div>{place && place.services}</div>
-              <div>Location: {place && place.location}</div>
-              <div>
-                <Rate allowHalf disabled value={place && place.averageRate} />
-              </div>
-            </div>}
-          />
-        </Card>
-        <Row gutter={[16, 16]} style={{ marginTop: 16 }}>
-          {posts && posts.map((p) => (
-            <Col className="gutter-row" key={p.id} span={24}>
+        <h2>Post</h2>
+        <Row className="show-grid" style={{ background: '#f8f3f3', padding: '10px'}}>
+        <Col xs={6} md={8} className="gutter-row">
+            <p><h3 style={{color: '#1890ff'}}>{post_owner && post_owner.username}</h3></p>
+        <i>{post && post.created_at}</i>
+        <p><i className="icon-map icons mt-4"/>{post && post.content}</p>
+        <Button variant="contained" color="primary" className="float-right" onClick={this.upVote}>
+        <LikeOutlined />
+        </Button>
+        </Col>
+        </Row>
+
+        <h3 style={{ marginTop: 16 }} >Comments</h3>
+        <Row gutter={[16, 16]} style={{ marginBottom: 16 }}>
+          {comments && comments.map((p) => (
+            <Col className="gutter-row" key={p.id} span={12}>
               <Card hoverable>
+              <i>{p.created_at}</i>
+
                 <Meta
-                  avatar={<Avatar size='large' icon={<UserOutlined />} />}
                   title={p.user.username}
                   description={p.content}
                 />
@@ -62,6 +72,15 @@ export default class Post extends Component {
             </Col>
           ))}
         </Row>
+
+      <Form>
+      <Form.Item name="comment">
+        <Input />
+      </Form.Item>
+      <Button type="primary" htmlType="submit">
+          Comment
+        </Button>
+      </Form>
       </div>
     )
   }
